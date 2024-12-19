1978f8318b80b701e6cb16514dcb6b00f4046fd9
idamato <61636703+idamato@users.noreply.github.com>
2024-12-19 10:40:12 +0100

    Update serial_data_logger.py
    visualizza correttamente display per debug

--- serial_data_logger.py
+++ serial_data_logger.py
@@ -3,27 +3,28 @@ def on_button_pressed_a():
     pin0 = 1
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+audio = 0
+temperatura = 0
+luce = 0
+sonar2 = 0
 pin0 = 0
 serial.redirect_to_usb()
-luce = 0
-temperatura = 0
-audio = 0
 pin0 = 0
-sonar2 = 0
 
 def on_forever():
-    global luce, temperatura, audio, sonar2, pin0
+    global sonar2, pin0, luce, temperatura, audio
     sonar2 = sonar.ping(DigitalPin.P12, DigitalPin.P13, PingUnit.CENTIMETERS)
     if sonar2 < 8:
         pin0 = 1
         luce = input.light_level()
         temperatura = input.temperature()
         audio = input.sound_level()
-        serial.write_line("" + str(luce) + "," + str(temperatura) + "," + str(audio) + "," + str(pin0) + "," + str(sonar2))
+        serial.write_line("" + str(luce) + "," + ("" + str(temperatura)) + "," + ("" + str(audio)) + "," + ("" + str(pin0)) + "," + ("" + str(sonar2)))
         basic.show_number(pin0)
         if pin0 == 1:
             pin0 = 0
             basic.pause(2000)
+            basic.clear_screen()
     else:
         basic.pause(200)
 basic.forever(on_forever)
