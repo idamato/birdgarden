3124d4d76e8c82cc2aed1cdc3fd3db5629510e3a
idamato <61636703+idamato@users.noreply.github.com>
2024-12-19 11:40:39 +0100

    Update serialandphoto.py
    inseriti i commenti per la gestione del led

--- serialandphoto.py
+++ serialandphoto.py
@@ -3,10 +3,22 @@
 from time import sleep
 from picamera2 import Picamera2
 import serial
+# from gpiozero import LED
+# from gpiozero import MotionSensor
 
 ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 camera = Picamera2()
+# l'anodo del LED (gamba lunga) è collegato al GPIO 17
+# fra il catodo (gamba corta) e GROUND è necessario collegare una resistenza di almeno 100 ohm
+# flash_led = LED(17)
+# nel caso di utilizzo di motion sensor
+# sensor = MotionSensor(14)
+
 id = 0
+# capture_config = camera.create_still_configuration(main={"size":(1920, 1080)}, lores={"size":(640,480)})
+capture_config = camera.create_still_configuration()
+# camera.configure(capture_config)
+
 
 print ("Hello this is the start!")
 
@@ -22,10 +34,15 @@ while True:
         id += 1
         print('fare foto!')
         camera.start()
-        sleep(2)
+        # capture_config = camera.create_still_configuration(main={"size":(1920, 1080)}, lores={"size":(640,480)})
         capture_config = camera.create_still_configuration()
         filename = "/home/ilfarodargento/software/departures/" + str(id) + "_" + str(luce) + "_" + str(temperatura) + "_" + str(audio) + "_" + str(sonar) + ".jpg"
+        # flash_led.on()
+        sleep(1)
+        # esegue la foto
         camera.switch_mode_and_capture_file(capture_config, filename)
+        # flash_led.off()
+        camera.stop()
         print('fatta foto: ', filename)
       # sleep(1)
 done
