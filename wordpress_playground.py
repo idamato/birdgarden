e61c3fb6433e2b1fc5ae4f5c49a7b66ecb00b55d
idamato <61636703+idamato@users.noreply.github.com>
2025-01-03 22:46:51 +0100

    Update wordpress_playground.py

--- wordpress_playground.py
+++ wordpress_playground.py
@@ -1,13 +1,18 @@
 from wordpress_scripts import wordpress_header
 from wordpress_scripts import postmedia
 from wordpress_scripts import create_wordpress_post
+import sys
+
+serial = sys.argv[0]
+sensor_data = sys.argv[1]
+media = sys.argv[2]
 
 wp_user = "INSERT-WORDPRESS-USERNAME"
 wp_pw = "INSERT-WORDPRESS-APPLICATION-PASSWORD"
 
 # Esepio da adeguare al contesto
 url = "www.webradiofaro.it/wp" #you set the root domain of your WordPress site
-media = "images/000000008279d33b.2025010232901.3_75_21_37_3.jpg" #you set the location of the image you want to upload
+# media = "images/000000008279d33b.2025010232901.3_75_21_37_3.jpg" #you set the location of the image you want to upload
 filename = "gazometro" #you set the name of your image
 title = "Il mio primo articolo generato con Python" #you set the title of your first WordPress post
 body = "Quando le immagini parlano è inutile spendere troppe parole..."
