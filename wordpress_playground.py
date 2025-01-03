031cfbfa5b193f62d9e787ff5e4ccc2dac13f453
idamato <61636703+idamato@users.noreply.github.com>
2025-01-03 23:25:31 +0100

    Update wordpress_playground.py

--- wordpress_playground.py
+++ wordpress_playground.py
@@ -2,22 +2,26 @@ from wordpress_scripts import wordpress_header
 from wordpress_scripts import postmedia
 from wordpress_scripts import create_wordpress_post
 import sys
+from datetime import datetime
 
-serial = sys.argv[0]
+now = datetime.now()
+formatted_date_time = now.strftime("%Y%m%d%H%M%S")
+
+seriale = sys.argv[0]
 sensor_data = sys.argv[1]
 media = sys.argv[2]
 
 wp_user = "INSERT-WORDPRESS-USERNAME"
 wp_pw = "INSERT-WORDPRESS-APPLICATION-PASSWORD"
 
-# Esepio da adeguare al contesto
+# Esempio da adeguare al contesto
 url = "www.webradiofaro.it/wp" #you set the root domain of your WordPress site
 # media = "images/000000008279d33b.2025010232901.3_75_21_37_3.jpg" #you set the location of the image you want to upload
-filename = sensor_data #you set the name of your image
+filename = seriale + "." + formatted_date_time + "." + sensor_data #you set the name of your image
 title = "Il mio primo articolo generato con Python" #you set the title of your first WordPress post
 body = "Quando le immagini parlano è inutile spendere troppe parole..."
 categories = [97] # birdgarden
-tags = [94] #identificare il tag con la CPUID del mittente, creato al momento dell'iscrizione
+tags = [94] #identificare il tag con la CPUID (seriale) del Raspberry mittente, creato al momento dell'iscrizione
 
 #authentication
 wordpress_head_post = wordpress_header(wp_user, wp_pw)[0]
