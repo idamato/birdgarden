9bce4c3996d114ad1e7707aaac765a1acf009e42
idamato <61636703+idamato@users.noreply.github.com>
2025-01-03 23:00:46 +0100

    Update wordpress_playground.py

--- wordpress_playground.py
+++ wordpress_playground.py
@@ -13,11 +13,11 @@ wp_pw = "INSERT-WORDPRESS-APPLICATION-PASSWORD"
 # Esepio da adeguare al contesto
 url = "www.webradiofaro.it/wp" #you set the root domain of your WordPress site
 # media = "images/000000008279d33b.2025010232901.3_75_21_37_3.jpg" #you set the location of the image you want to upload
-filename = "gazometro" #you set the name of your image
+filename = sensor_data #you set the name of your image
 title = "Il mio primo articolo generato con Python" #you set the title of your first WordPress post
 body = "Quando le immagini parlano è inutile spendere troppe parole..."
-categories = [97] #11 being for instance the "Coding" category on your site
-tags = [94] #2 being for instance the "Python" tag on your site
+categories = [97] # birdgarden
+tags = [94] #identificare il tag con la CPUID del mittente, creato al momento dell'iscrizione
 
 #authentication
 wordpress_head_post = wordpress_header(wp_user, wp_pw)[0]
