999b94478cbd238b978976bd41d51ba66fcb6f46
idamato <61636703+idamato@users.noreply.github.com>
2025-01-04 17:47:07 +0100

    Update wordpress_playground.py

--- wordpress_playground.py
+++ wordpress_playground.py
@@ -4,12 +4,12 @@ from wordpress_scripts import create_wordpress_post
 import sys
 from datetime import datetime
 
-now = datetime.now()
-formatted_date_time = now.strftime("%Y%m%d%H%M%S")
+#now = datetime.now()
+#formatted_date_time = now.strftime("%Y%m%d%H%M%S")
 
-seriale = sys.argv[0]
-datafile = sys.argv[1]
-media = sys.argv[2]
+seriale = sys.argv[1]
+datafile = sys.argv[2]
+media = sys.argv[3]
 
 wp_user = "INSERT-WORDPRESS-USERNAME"
 wp_pw = "INSERT-WORDPRESS-APPLICATION-PASSWORD"
@@ -18,10 +18,15 @@ wp_pw = "INSERT-WORDPRESS-APPLICATION-PASSWORD"
 url = "www.webradiofaro.it/wp" #you set the root domain of your WordPress site
 # media = "images/000000008279d33b.2025010232901.3_75_21_37_3.jpg" #you set the location of the image you want to upload
 filename = seriale + "." + datafile #you set the name of your image
-title = "Il mio primo articolo generato con Python" #you set the title of your first WordPress post
+title = "Foto scattata dal Birdgarden " + seriale #you set the title of your first WordPress post
 body = "Quando le immagini parlano è inutile spendere troppe parole..."
 categories = [97] # birdgarden
 tags = [94] #identificare il tag con la CPUID (seriale) del Raspberry mittente, creato al momento dell'iscrizione
+
+#debug
+print("URL: " + url)
+print("Media: " + media)
+print("Filename: " + filename)
 
 #authentication
 wordpress_head_post = wordpress_header(wp_user, wp_pw)[0]
