eefa84889da4bfa7e303da8fb47020aecc43e4f3
idamato <61636703+idamato@users.noreply.github.com>
2025-03-17 22:33:20 +0100

    Update serial_data_logger.py

--- serial_data_logger.py
+++ serial_data_logger.py
@@ -1,29 +1,57 @@
+# Scatta la foto
+
 def on_button_pressed_a():
-    global pin0
-    pin0 = 1
+    global trig0
+    trig0 = 1
+    basic.show_number(trig0)
+    basic.pause(200)
+    basic.clear_screen()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+# Invia il segnale di halt al RPi
+
+def on_logo_long_pressed():
+    serial.write_line("0,0,0,0,0")
+    basic.pause(200)
+    serial.write_line("0,0,0,0,0")
+input.on_logo_event(TouchButtonEvent.LONG_PRESSED, on_logo_long_pressed)
+
+# Scatta la foto
+
+def on_button_pressed_b():
+    global trig0
+    trig0 = 2
+    basic.show_number(trig0)
+    basic.pause(200)
+    basic.clear_screen()
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def rileva():
+    global luce, temperatura, audio
+    luce = input.light_level()
+    temperatura = input.temperature()
+    audio = input.sound_level()
+    
+sonar2 = 0
 audio = 0
 temperatura = 0
 luce = 0
-sonar2 = 0
-pin0 = 0
+trig0 = 0
 serial.redirect_to_usb()
-pin0 = 0
+trig0 = 0
 
 def on_forever():
-    global sonar2, pin0, luce, temperatura, audio
-    sonar2 = sonar.ping(DigitalPin.P12, DigitalPin.P13, PingUnit.CENTIMETERS)
-    if sonar2 < 12:
-        pin0 = 1
-        basic.show_number(pin0)
-        luce = input.light_level()
-        temperatura = input.temperature()
-        audio = input.sound_level()
-        serial.write_line("" + str(luce) + "," + ("" + str(temperatura)) + "," + ("" + str(audio)) + "," + ("" + str(pin0)) + "," + ("" + str(sonar2)))
+    global sonar2, trig0
+    sonar2 = sonar.ping(DigitalPin.P0, DigitalPin.P1, PingUnit.CENTIMETERS)
+    if sonar2 < 20:
+        trig0 = 1
+        basic.show_number(trig0)
+        rileva()
+        serial.write_line("" + str(luce) + "," + ("" + str(temperatura)) + "," + ("" + str(audio)) + "," + ("" + str(trig0)) + "," + ("" + str(sonar2)))
         basic.clear_screen()
-        pin0 = 0
+        trig0 = 0
         basic.pause(500)
     else:
         basic.pause(200)
 basic.forever(on_forever)
+
