c67df88722287b4e4331e14772fd70fbb6178a1c
idamato <61636703+idamato@users.noreply.github.com>
2025-03-23 13:27:27 +0100

    Update serial_data_logger.py

--- serial_data_logger.py
+++ serial_data_logger.py
@@ -1,13 +1,5 @@
-# Scatta la foto
-
-def on_button_pressed_a():
-    global trig0
-    trig0 = 1
-    basic.show_number(trig0)
-    basic.pause(200)
-    basic.clear_screen()
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
+def scriviseriale():
+    serial.write_line("" + str(luce) + "," + ("" + str(temperatura)) + "," + ("" + str(audio)) + "," + ("" + str(trig0)) + "," + ("" + str(sonar2)))
 # Invia il segnale di halt al RPi (soluzione per spegnerlo)
 
 def on_logo_long_pressed():
@@ -16,22 +8,11 @@ def on_logo_long_pressed():
     serial.write_line("0,0,0,0,0")
 input.on_logo_event(TouchButtonEvent.LONG_PRESSED, on_logo_long_pressed)
 
-# Scatta la foto
-
-def on_button_pressed_b():
-    global trig0
-    trig0 = 2
-    basic.show_number(trig0)
-    basic.pause(200)
-    basic.clear_screen()
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
 def rileva():
     global luce, temperatura, audio
     luce = input.light_level()
     temperatura = input.temperature()
     audio = input.sound_level()
-    
 sonar2 = 0
 audio = 0
 temperatura = 0
@@ -47,11 +28,12 @@ def on_forever():
         trig0 = 1
         basic.show_number(trig0)
         rileva()
-        serial.write_line("" + str(luce) + "," + ("" + str(temperatura)) + "," + ("" + str(audio)) + "," + ("" + str(trig0)) + "," + ("" + str(sonar2)))
+        scriviseriale()
         basic.clear_screen()
         trig0 = 0
-        basic.pause(500)
+        basic.pause(10000)
     else:
-        basic.pause(200)
+        basic.pause(500)
 basic.forever(on_forever)
+
 
