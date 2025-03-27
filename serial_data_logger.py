624a5c1a61395effe77703514ade3a8d830fc6a5
idamato <61636703+idamato@users.noreply.github.com>
2025-03-27 21:30:42 +0100

    Update serial_data_logger.py

--- serial_data_logger.py
+++ serial_data_logger.py
@@ -1,18 +1,4 @@
-def scriviseriale():
-    serial.write_line("" + str(luce) + "," + ("" + str(temperatura)) + "," + ("" + str(audio)) + "," + ("" + str(trig0)) + "," + ("" + str(sonar2)))
 # Invia il segnale di halt al RPi (soluzione per spegnerlo)
-
-def on_logo_long_pressed():
-    serial.write_line("0,0,0,0,0")
-    basic.pause(200)
-    serial.write_line("0,0,0,0,0")
-input.on_logo_event(TouchButtonEvent.LONG_PRESSED, on_logo_long_pressed)
-
-def rileva():
-    global luce, temperatura, audio
-    luce = input.light_level()
-    temperatura = input.temperature()
-    audio = input.sound_level()
 sonar2 = 0
 audio = 0
 temperatura = 0
@@ -21,6 +7,20 @@ trig0 = 0
 serial.redirect_to_usb()
 trig0 = 0
 
+def on_logo_long_pressed():
+    global trig0
+    serial.write_line("0,0,0,0,0")
+    basic.pause(200)
+    serial.write_line("0,0,0,0,0")
+    trig0 = 0
+input.on_logo_event(TouchButtonEvent.LONG_PRESSED, on_logo_long_pressed)
+
+def rileva():
+    global luce, temperatura, audio
+    luce = input.light_level()
+    temperatura = input.temperature()
+    audio = input.sound_level()
+    
 def on_forever():
     global sonar2, trig0
     sonar2 = sonar.ping(DigitalPin.P0, DigitalPin.P1, PingUnit.CENTIMETERS)
@@ -28,12 +28,10 @@ def on_forever():
         trig0 = 1
         basic.show_number(trig0)
         rileva()
-        scriviseriale()
+        serial.write_line("" + str(luce) + "," + ("" + str(temperatura)) + "," + ("" + str(audio)) + "," + ("" + str(trig0)) + "," + ("" + str(sonar2)))
         basic.clear_screen()
         trig0 = 0
         basic.pause(10000)
     else:
         basic.pause(500)
 basic.forever(on_forever)
-
-
