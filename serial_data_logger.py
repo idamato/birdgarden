93c904da0bb9c71c584e224c53e37a97942d3f02
idamato <61636703+idamato@users.noreply.github.com>
2025-04-09 21:10:02 +0200

    Update serial_data_logger.py

--- serial_data_logger.py
+++ serial_data_logger.py
@@ -1,4 +1,17 @@
-# Invia il segnale di halt al RPi (soluzione per spegnerlo)
+def on_logo_pressed():
+    global trig0
+    serial.write_line("0,0,0,0,0")
+    basic.show_icon(IconNames.NO)
+    basic.pause(1000)
+    serial.write_line("0,0,0,0,0")
+    trig0 = 0
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
+
+def rileva():
+    global luce, temperatura, audio
+    luce = input.light_level()
+    temperatura = input.temperature()
+    audio = input.sound_level()
 sonar2 = 0
 audio = 0
 temperatura = 0
@@ -6,32 +19,20 @@ luce = 0
 trig0 = 0
 serial.redirect_to_usb()
 trig0 = 0
+basic.show_icon(IconNames.HEART)
+basic.pause(2000)
 
-def on_logo_long_pressed():
-    global trig0
-    serial.write_line("0,0,0,0,0")
-    basic.pause(200)
-    serial.write_line("0,0,0,0,0")
-    trig0 = 0
-input.on_logo_event(TouchButtonEvent.LONG_PRESSED, on_logo_long_pressed)
-
-def rileva():
-    global luce, temperatura, audio
-    luce = input.light_level()
-    temperatura = input.temperature()
-    audio = input.sound_level()
-    
 def on_forever():
     global sonar2, trig0
     sonar2 = sonar.ping(DigitalPin.P0, DigitalPin.P1, PingUnit.CENTIMETERS)
-    if sonar2 < 15:
+    if sonar2 < 18:
         trig0 = 1
         basic.show_number(trig0)
         rileva()
         serial.write_line("" + str(luce) + "," + ("" + str(temperatura)) + "," + ("" + str(audio)) + "," + ("" + str(trig0)) + "," + ("" + str(sonar2)))
         basic.clear_screen()
         trig0 = 0
-        basic.pause(10000)
+        basic.pause(2000)
     else:
         basic.pause(500)
 basic.forever(on_forever)
